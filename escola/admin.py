280a30a5c31e78cea9737de91397c03f514cc795
William Santos <Willsantos.edf@gmail.com>
2023-01-27 22:04:28 -0300

    criando serializer.

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from escola.models import Aluno, Curso
+
+class AlunosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'rg', 'cpf', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+    list_per_page = 20
+
+admin.site.register(Aluno, AlunosAdmin)
+
+
+class CursosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo_curso', 'descricao')
+    list_display_links = ('id', 'codigo_curso')
+    search_fields = ('codigo_curso',)
+
+admin.site.register(Curso,CursosAdmin)
